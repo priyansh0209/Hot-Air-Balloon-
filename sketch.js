3dce7d7733d4459b7cf240b34d464e84ecaa7799
priyansh0209 <81817605+priyansh0209@users.noreply.github.com>
2021-05-30 10:51:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+var balloon,balloonImage1,balloonImage2;
+var database,position,balloonposition;
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  balloonposition = database.ref("Balloon/position");
+  balloonposition.on("value",readPosition)
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    balloon.scale=balloon.scale+0.01;
+    changePosition(-10,0);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    balloon.scale=balloon.scale-0.01;
+    changePosition(+10,0);
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    balloon.scale=balloon.scale-0.01;
+    changePosition(0,-10);
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    balloon.scale=balloon.scale+0.01;
+    changePosition(0,+10);
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+function changePosition(x,y){
+ database.ref("Balloon/position").set({
+     "x":position.x+x,
+     "y":position.y+y
+  })
+ }
+
+ function readPosition(data){
+   position = data.val();
+   balloon.x = position.x
+   balloon.y = position.y
+ }
+
